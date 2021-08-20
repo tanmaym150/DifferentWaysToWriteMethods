f00fcf3f5653f6e15fa1166e0ef3bd3edb5a23a3
tanmaym150 <tanmaymahamulkar150@gmail.com>
2021-08-20 18:42:18 +0530

    Add project files.

---
+++ DifferentWaysToWriteMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DifferentWaysToWriteMethods
+{
+    class Program
+    {
+        
+        static int Sum(int x,int y)//static method with parameter
+        {
+            int a = x;
+            int b = y;
+
+            int result = a + b;
+            return result;
+        }
+        public void Greet()//instance method
+        {
+            Console.WriteLine("Hello interns..");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            int r = Program.Sum(10,20);
+            Console.WriteLine(r);
+            p.Greet();
+            
+        }
+    }
+}
